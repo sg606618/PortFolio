792726b4c35f6bfa90fd96579209048e3350eebe
Santosh Ghimire <sg606618@gmail.com>
2023-11-30 19:06:14 +0545

    changes

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,11 +1,11 @@
-import ProfileImg from "next/image";
+import Image from "next/image";
 import React from "react";
 
 const Background = () => {
   return (
     <>
       <div className="absolute top-0 right-0 -z-20">
-        <img
+        <Image
           src={"./images/ProfileImage.png"}
           alt="profile image"
           className="saturate-100 grayscale-[20%] contrast-150 brightness-50"
